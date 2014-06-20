d9471c9b5c92e77d76dd6f792fcf78bf6675e3d8
Chris Moschini <chris@brass9.com>
2014-06-20 11:04:21 -0700

    Add more plugins, minify makeEditable().

--- Editor/Editor.js
+++ Editor/Editor.js
@@ -1,12 +1,27 @@
 goog.provide('Editor');
 
+goog.require('goog.editor.Command');
 goog.require('goog.editor.SeamlessField');
+goog.require('goog.editor.plugins.BasicTextFormatter');
+goog.require('goog.editor.plugins.EnterHandler');
 
 window['Editor'] = goog.editor.SeamlessField;
 
 var p = goog.editor.SeamlessField.prototype;
 
-p['makeEditable'] = p.makeEditable;
+//p['makeEditable'] = p.makeEditable;	// We always call this, so call this from minified code instead
 p['isModified'] = p.isModified;
 p['manipulateDom'] = p.manipulateDom;
 p['getHtml'] = p.getCleanContents;	// Renamed
+
+// Outside code can call makeEditor('editor') to switch on a seamless field and make it editable, with plugins enabled.
+window['makeEditor'] = function (id) {
+	var editor = new goog.editor.SeamlessField(id);
+	editor.makeEditable();
+
+	editor.registerPlugin(new goog.editor.plugins.BasicTextFormatter());
+	editor.registerPlugin(new goog.editor.plugins.EnterHandler());
+
+	return editor;
+};
+
